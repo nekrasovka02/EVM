616e555d4111043291e8136cdc2299aa9b018007
nekrasovka02 <125794222+nekrasovka02@users.noreply.github.com>
2023-09-19 16:57:08 +0400

    React website (#2)
    * commit
    
    * init projuct
    
    * fix project 1
    
    * merge master

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-photo/isolated-black-t-shirt-model-front-view_125540-1073.jpg?w=1060&t=st=1695126862~exp=1695127462~hmac=170f69d32671d3c904f05ac36910ad78be44936c7fad88e341b406a0061c2c42",
+        title: "title 1",
+        desc: "desc 1",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://img.freepik.com/free-photo/woman-wearing-blank-shirt_23-2149347516.jpg?w=1060&t=st=1695126945~exp=1695127545~hmac=91f346dd745d39ea93897fe7b141bc2c453bb7b891432b9bef375b65007bc6b2",
+        title: "title 2",
+        desc: "desc 2",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://img.freepik.com/free-photo/cute-redhead-boy-with-good-looking-hairstyle-in-white-t-shirt-holding-holding-hands-in-pockets-gently-smiling_176420-15463.jpg?w=1060&t=st=1695126988~exp=1695127588~hmac=a60add17fbdb7a2b1efe8efcaa82f63f1ef3493e0023dbb80ba48327e39c5141",
+        title: "title 3",
+        desc: "desc 3",
+        bg: "fbf4f0",
+    }
+
+]
+    
+export const categories = [
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-photo/isolated-black-t-shirt-model-front-view_125540-1073.jpg?w=1060&t=st=1695126862~exp=1695127462~hmac=170f69d32671d3c904f05ac36910ad78be44936c7fad88e341b406a0061c2c42",
+        title: "title 1",
+    },
+    {
+        id: 2,
+        img: "https://img.freepik.com/free-photo/woman-wearing-blank-shirt_23-2149347516.jpg?w=1060&t=st=1695126945~exp=1695127545~hmac=91f346dd745d39ea93897fe7b141bc2c453bb7b891432b9bef375b65007bc6b2",
+        title: "title 2",
+    },
+    {
+        id: 3,
+        img: "https://img.freepik.com/free-photo/cute-redhead-boy-with-good-looking-hairstyle-in-white-t-shirt-holding-holding-hands-in-pockets-gently-smiling_176420-15463.jpg?w=1060&t=st=1695126988~exp=1695127588~hmac=a60add17fbdb7a2b1efe8efcaa82f63f1ef3493e0023dbb80ba48327e39c5141",
+        title: "title 3",
+    }
+    
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/72b/1008_800_1/100241060299.jpg",
+    },
+    {
+        id: 2,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/3aa/1008_800_1/79815220299.jpg",
+    },
+    {
+        id: 3,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/3d7/1008_800_1/84239250299.jpg",
+    },
+    {
+        id: 4,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/ec7/1008_800_1/60335640299.jpg",
+    },
+    {
+        id: 5,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/b3f/1008_800_1/81090600299.jpg",
+    },
+    {
+        id: 6,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/be7/1008_800_1/63933060299.jpg",
+    },
+    {
+        id: 7,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/347/1008_800_1/70365800299.jpg",
+    },
+    {
+        id: 8,
+        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/a3e/1008_800_1/77525140299.jpg",
+    }
+]
