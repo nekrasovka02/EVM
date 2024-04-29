6636db3e8011dab28484aa1a82eda34fa90f8bfb
nekrasovka02 <125794222+nekrasovka02@users.noreply.github.com>
2024-04-29 23:07:02 +0400

    create home page (#3)

--- src/data.js
+++ src/data.js
@@ -45,34 +45,34 @@ export const categories = [
 export const popularProducts = [
     {
         id: 1,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/72b/1008_800_1/100241060299.jpg",
+        img:"https://img.freepik.com/free-photo/mid-shot-man-posing-amiling_23-2148618705.jpg?t=st=1714410096~exp=1714413696~hmac=4314d908c35c3645cea7c254c55ef14b0cd926d4b42839f6dd91cecf6d0ec07f&w=740",
     },
     {
         id: 2,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/3aa/1008_800_1/79815220299.jpg",
+        img:"https://img.freepik.com/free-photo/medium-shot-man-with-sunglasses-posing_23-2148703193.jpg?t=st=1714410180~exp=1714413780~hmac=923b09ce5562dbd6a1d8d4c37c0c50b314687b1eefcdef259018cbf2f0105de9&w=740",
     },
     {
         id: 3,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/3d7/1008_800_1/84239250299.jpg",
+        img:"https://img.freepik.com/free-photo/casual-dressed-man-sitting-sidewalk-outdoor-photoshoot_53876-101177.jpg?t=st=1714410256~exp=1714413856~hmac=27586190593212b18a8b9e3808f5184f4c9ec5c235a0d74ccb226612ac4f0225&w=740",
     },
     {
         id: 4,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/ec7/1008_800_1/60335640299.jpg",
+        img:"https://img.freepik.com/free-photo/woman-red-striped-crop-top-fashion-shoot_53876-102199.jpg?t=st=1714410277~exp=1714413877~hmac=496f19f498484ba5f23181668166cf445273d5e7ed5e1dec3aacbfc21552d47c&w=740",
     },
     {
         id: 5,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/b3f/1008_800_1/81090600299.jpg",
+        img:"https://img.freepik.com/free-photo/portrait-young-woman-holding-disposable-coffee-cup-hand-sitting-caf_23-2148148223.jpg?t=st=1714410298~exp=1714413898~hmac=e40cfa2fbcca9c624ccebece79d9cefd7d2d3bd3438936063db3390ffd5dffd0&w=740",
     },
     {
         id: 6,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/be7/1008_800_1/63933060299.jpg",
+        img:"https://img.freepik.com/free-photo/deaf-woman-communicating-through-sign-language_23-2148590363.jpg?t=st=1714410312~exp=1714413912~hmac=cb86e4b996e643748bca31a878a35e7ee2b3b0d1d683b1d8b1661866686dd8d9&w=740",
     },
     {
         id: 7,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/347/1008_800_1/70365800299.jpg",
+        img:"https://img.freepik.com/free-photo/portrait-happy-young-woman-green-whatsapp-t-shirt_23-2147842170.jpg?t=st=1714410336~exp=1714413936~hmac=62eef4eb7d07a3cbeab45af715345fcb62ee804bd817a6fdcf8570aeac8f022b&w=740",
     },
     {
         id: 8,
-        img:"https://cdn.sportmaster.ru/upload/resize_cache/iblock/a3e/1008_800_1/77525140299.jpg",
+        img:"https://img.freepik.com/free-photo/front-view-woman-stretching_23-2148784620.jpg?t=st=1714410374~exp=1714413974~hmac=ea32a4248dab89e93c2466c4ffd0a487aee76d6dfe25fee8523d587a4e30e541&w=740",
     }
 ]
